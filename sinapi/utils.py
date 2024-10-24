ebd3865179f02d836531504a81f4280824985e6f
Antonio Fernando <antoniofernandodj@outlook.com>
2024-10-24 16:26:09 -0300

    *

--- sinapi/utils.py
+++ sinapi/utils.py
@@ -25,7 +25,7 @@ async def get_estados_a_cadastrar(service: SinapiService):
 
     estados_disponiveis = set(estados_response.items)
     print('estados_disponiveis')
-    print(estados_disponiveis)
+    print([estado.uf for estado in estados_disponiveis])
 
     with Session() as session:
         estados_cadastrados_sqla = session.query(Estado).all()
@@ -36,15 +36,15 @@ async def get_estados_a_cadastrar(service: SinapiService):
 
     ultimo_estado = estados_cadastrados_list[-1]
     print('ultimo_estado')
-    print(ultimo_estado)
+    print(ultimo_estado.uf)
 
     estados_cadastrados = set(estados_cadastrados_list)
     print('estados_cadastrados')
-    print(estados_cadastrados)
+    print([estado.uf for estado in estados_cadastrados])
 
     estados_a_cadastrar = list(estados_disponiveis.difference(estados_cadastrados))
     estados_a_cadastrar.append(ultimo_estado)
     print('estados_a_cadastrar')
-    print(estados_a_cadastrar)
+    print([estado.uf for estado in estados_a_cadastrar])
 
     return estados_a_cadastrar
