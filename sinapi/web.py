9f7b6d6adf472da79ce6db4c349a0ccc952a63e2
Antonio Fernando <antoniofernandodj@outlook.com>
2024-10-28 21:41:56 -0300

    *

--- sinapi/web.py
+++ sinapi/web.py
@@ -38,6 +38,7 @@ async def get_insumos_or_compositions(
                         uf=uf,
                         composicao=composicao,
                     ):
+                        print(f'I: {insumo_response}')
                         yield (insumo_response, estado_response)
 
 
