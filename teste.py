99252e9d5119a4fa872b45fe5b84b8c458bff794
Antonio Fernando <antonio.dias@itemize.com.br>
2024-10-31 16:22:37 -0300

    *

--- teste.py
+++ teste.py
@@ -10,33 +10,37 @@ except:
     from models import InsumoComposicaoTabela, ComposicaoItem  # type: ignore
 
 
-SessionLocal = get_session_local(DATABASE_INSUMOS_URL2, echo=False)
+def main():
+    SessionLocal = get_session_local(DATABASE_INSUMOS_URL2, echo=False)
+
+    with SessionLocal() as session:
+
+        T1 = InsumoComposicaoTabela
+        stmt = select(T1.id)
+        ids = [row[0] for row in session.execute(stmt)]
+
+        for id in ids:
+            T2 = InsumoComposicaoTabela
+            pai = session.query(T2).filter_by(id=id).first()
+
+            assert pai
+
+            print(f"filhos: {pai.itens_filho}")
+
+            # pai = (
+            #     session.query(InsumoComposicaoTabela).filter_by(id=vinculo.id_insumo).all()
+            # )
+
+            # filho = (
+            #     session.query(InsumoComposicaoTabela)
+            #     .filter_by(id=vinculo.id_insumo_item)
+            #     .all()
+            # )
+
+            # print({"pai": pai, "filho": filho})
+
+        # print_tree(item)
 
 
-with SessionLocal() as session:
-
-    T1 = InsumoComposicaoTabela
-    stmt = select(T1.id)
-    ids = [row[0] for row in session.execute(stmt)]
-
-    for id in ids:
-        T2 = InsumoComposicaoTabela
-        pai = session.query(T2).filter_by(id=id).first()
-
-        assert pai
-
-        print(f"filhos: {pai.itens_filho}")
-
-        # pai = (
-        #     session.query(InsumoComposicaoTabela).filter_by(id=vinculo.id_insumo).all()
-        # )
-
-        # filho = (
-        #     session.query(InsumoComposicaoTabela)
-        #     .filter_by(id=vinculo.id_insumo_item)
-        #     .all()
-        # )
-
-        # print({"pai": pai, "filho": filho})
-
-    # print_tree(item)
+if __name__ == "__main__":
+    main()
