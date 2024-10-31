fdd8331dd97133014ff223ae5c64ed0d51c37f14
Antonio Fernando <antonio.dias@itemize.com.br>
2024-10-31 19:37:04 -0300

    linting

--- sinapi/web.py
+++ sinapi/web.py
@@ -8,21 +8,18 @@ try:
     from sinapi.api import SinapiService
     from sinapi.utils import get_estados_a_cadastrar
 except:
-    from api import SinapiService
+    from api import SinapiService  # type: ignore
     from api.schema import EstadoResponseItem, InsumosResponse
     from utils import get_estados_a_cadastrar
 
 login = "lsouza17@gmail.com"
 senha = "eflEs2cF"
 
-meses = [
-    Mes(value=i, text=str(i)) for i in range(1, 13)
-]
+meses = [Mes(value=i, text=str(i)) for i in range(1, 13)]
 
 
 async def get_insumos_or_compositions(
-    ano: str,
-    composicao=True
+    ano: str, composicao=True
 ) -> AsyncGenerator[tuple[InsumosResponse, EstadoResponseItem], Any]:
     service = SinapiService(login, senha)
     estados_response = await get_estados_a_cadastrar(service)
@@ -31,18 +28,20 @@ async def get_insumos_or_compositions(
         for estado_response in estados_response:
             uf = estado_response.uf
             for mes in meses:
-                if mes.value != 9:
-                    continue
-                
-                print(f'Buscando para {ano}, {mes.value}, {uf}, {composicao}')
+
+                print(f"Buscando para {ano}, {mes.value}, {uf}, {composicao}")
                 async for insumo_response in service.insumos_todos(
-                    ano=ano, mes=mes.value, uf=uf, composicao=composicao,
+                    ano=ano,
+                    mes=mes.value,
+                    uf=uf,
+                    composicao=composicao,
                 ):
 
                     yield (insumo_response, estado_response)
 
     except Exception as error:
         import traceback
+
         traceback.print_exc()
         print(error)
         print(type(error))
