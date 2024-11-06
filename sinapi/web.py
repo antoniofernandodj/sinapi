eab83215ffd0e80aa7e075f81860c687a4319baf
Antonio Fernando <antonio.dias@itemize.com.br>
2024-11-06 17:28:39 -0300

    *

--- sinapi/web.py
+++ sinapi/web.py
@@ -63,7 +63,7 @@ async def get_insumos_or_compositions(composicao=None):
                     print(
                         f"Buscando para ano: {ano}, mes: {mes}, uf: {uf}, {composicao}"
                     )
-                    async for item in service.insumos_todos(
+                    async for item in service.insumos_todos2(
                         ano=ano,
                         mes=mes,
                         uf=uf,
