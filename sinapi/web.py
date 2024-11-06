22c47fafaedc9ed19f5ae3f7dce9555f15cfa78d
Antonio Fernando <antonio.dias@itemize.com.br>
2024-11-06 15:07:48 -0300

    *

--- sinapi/web.py
+++ sinapi/web.py
@@ -20,37 +20,35 @@ login = "teste@teste.com.br"
 senha = "teste"
 
 
-meses = [mes for mes in range(1, 13)]
-
-ufs = [
+ufs = {
     # "AC",
     # "AL",
     # "AP",
     # "AM",
-    # "BA",
-    "CE",
-    "DF",
-    "ES",
-    "GO",
-    "MA",
-    "MT",
-    "MS",
-    "MG",
-    "PA",
-    "PB",
-    "PR",
-    "PE",
-    "PI",
-    "RJ",
-    "RN",
-    "RS",
-    "RO",
-    "RR",
-    "SC",
-    "SP",
-    "SE",
-    "TO",
-]
+    "BA": [1],
+    "CE": list(range(1, 13)),
+    "DF": list(range(1, 13)),
+    "ES": list(range(1, 13)),
+    "GO": list(range(1, 13)),
+    "MA": list(range(1, 13)),
+    "MT": list(range(1, 13)),
+    "MS": list(range(1, 13)),
+    "MG": list(range(1, 13)),
+    "PA": list(range(1, 13)),
+    "PB": list(range(1, 13)),
+    "PR": list(range(1, 13)),
+    "PE": list(range(1, 13)),
+    "PI": list(range(1, 13)),
+    "RJ": list(range(1, 13)),
+    "RN": list(range(1, 13)),
+    "RS": list(range(1, 13)),
+    "RO": list(range(1, 13)),
+    "RR": list(range(1, 13)),
+    "SC": list(range(1, 13)),
+    "SP": list(range(1, 13)),
+    "SE": list(range(1, 13)),
+    "TO": list(range(1, 13)),
+}
 
 
 async def get_insumos_or_compositions(composicao=True):
@@ -59,8 +57,8 @@ async def get_insumos_or_compositions(composicao=True):
     service = SinapiService(login, senha)
 
     try:
-        for mes in meses:
-            for uf in ufs:
+        for uf, meses in ufs.items():
+            for mes in meses:
                 print(f"Buscando para ano: {ano}, mes: {mes}, uf: {uf}, {composicao}")
                 async for item in service.insumos_todos2(
                     ano=ano,
