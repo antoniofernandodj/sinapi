93b88e18ca2d26662fb2b07211574f7233542654
Antonio Fernando <antonio.dias@itemize.com.br>
2024-11-06 14:56:38 -0300

    *

--- sinapi/web.py
+++ sinapi/web.py
@@ -13,10 +13,44 @@ except:
     from api.schema import EstadoResponseItem, InsumosResponse
     from utils import get_estados_a_cadastrar
 
-login = "lsouza17@gmail.com"
-senha = "eflEs2cF"
+# login = "lsouza17@gmail.com"
+# senha = "eflEs2cF"
 
-# meses = [Mes(value=i, text=str(i)) for i in range(1, 13)]
+login = "teste@teste.com.br"
+senha = "teste"
+
+
+meses = [mes for mes in range(1, 13)]
+
+ufs = [
+    "AC",
+    "AL",
+    "AP",
+    "AM",
+    "BA",
+    "CE",
+    "DF",
+    "ES",
+    "GO",
+    "MA",
+    "MT",
+    "MS",
+    "MG",
+    "PA",
+    "PB",
+    "PR",
+    "PE",
+    "PI",
+    "RJ",
+    "RN",
+    "RS",
+    "RO",
+    "RR",
+    "SC",
+    "SP",
+    "SE",
+    "TO",
+]
 
 
 async def get_insumos_or_compositions(composicao=True):
@@ -24,69 +58,18 @@ async def get_insumos_or_compositions(composicao=True):
     ano = str(datetime.date.today().year)
     service = SinapiService(login, senha)
 
-    # estados_response = await get_estados_a_cadastrar(service)
-
-    # try:
-    #     for estado_response in estados_response:
-    #         uf = estado_response.uf
-    #         for mes in meses:
-
-    #             print(f"Buscando para {ano}, {mes.value}, {uf}, {composicao}")
-    #             async for insumo_response in service.insumos_todos(
-    #                 ano=ano,
-    #                 mes=mes.value,
-    #                 uf=uf,
-    #                 composicao=composicao,
-    #             ):
-
-    #                 yield (insumo_response, estado_response)
     try:
-
-        """
-        estados_meses = [
-            (1, "MG"),
-            (2, "MG"),
-            (3, "MG"),
-            (4, "MG"),
-            (5, "MG"),
-            (6, "MG"),
-            (7, "MG"),
-            (1, "MS"),
-            (2, "MS"),
-            (3, "MS"),
-            (4, "MS"),
-            (5, "MS"),
-            (6, "MS"),
-            (7, "MS"),
-            (8, "MS"),
-            (1, "RS"),
-            (2, "RS"),
-            (3, "RS"),
-            (4, "RS"),
-            (5, "RS"),
-            (6, "RS"),
-        ]
-        """
-
-        estados_meses = [
-            (1, "SP"),
-            (2, "SP"),
-            (3, "SP"),
-            (4, "SP"),
-            (5, "SP"),
-            (6, "SP"),
-        ]
-
-        for mes, uf in estados_meses:
-            print(f"Buscando para ano: {ano}, mes: {mes}, uf: {uf}, {composicao}")
-            async for item in service.insumos_todos2(
-                ano=ano,
-                mes=mes,
-                uf=uf,
-                composicao=composicao,
-            ):
-                # print(item)
-                yield item
+        for mes in meses:
+            for uf in ufs:
+                print(f"Buscando para ano: {ano}, mes: {mes}, uf: {uf}, {composicao}")
+                async for item in service.insumos_todos2(
+                    ano=ano,
+                    mes=mes,
+                    uf=uf,
+                    composicao=composicao,
+                ):
+                    # print(item)
+                    yield item
 
     except Exception as error:
         import traceback
